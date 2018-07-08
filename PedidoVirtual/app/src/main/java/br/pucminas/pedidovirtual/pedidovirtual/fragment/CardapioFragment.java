ecb4256664d4d0d5ea8c46b8cf55cd9c4a921981
pablo <pablo.souza18@hotmail.com>
2018-07-08 20:14:38 -0300

    Pedido Virtual

--- PedidoVirtual/app/src/main/java/br/pucminas/pedidovirtual/pedidovirtual/fragment/CardapioFragment.java
+++ PedidoVirtual/app/src/main/java/br/pucminas/pedidovirtual/pedidovirtual/fragment/CardapioFragment.java
@@ -3,21 +3,23 @@ package br.pucminas.pedidovirtual.pedidovirtual.fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import br.pucminas.pedidovirtual.pedidovirtual.R;
 import br.pucminas.pedidovirtual.pedidovirtual.activity.EstabelecimentoActivity;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class CardapioFragment extends Fragment {
 
+    @BindView(R.id.RV_prato)
+    RecyclerView RV_prato;
+
     private EstabelecimentoActivity activity;
-
-    public CardapioFragment(){
-
-    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
